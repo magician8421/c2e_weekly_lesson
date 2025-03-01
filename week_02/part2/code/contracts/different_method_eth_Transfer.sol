4c8b1c4732115a089ef8050f2e8db7b3b9a6b774
xiaotong <83378122@163.com>
2025-03-01 18:03:14 +0800

    init week2 part2

---
+++ week_02/part2/code/contracts/different_method_eth_Transfer.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+import "hardhat/console.sol";
+
+contract Called{
+    function balance() external view  returns(uint256){
+        return address(this).balance;
+    }
+    receive() external payable { 
+        console.log(address(this));
+    }
+}
+
+
+contract Caller{
+
+
+    address payable  called;
+
+    constructor(address payable to){
+        called=to;
+    }
+
+    function sender() external payable {
+        bool result=called.send(3);
+        require(result);
+    }
+
+    function transfer() external payable{
+        called.transfer(3);
+    }
+
+    function call() external payable{
+       (bool result,)=called.call{value:3}("");
+       require(result);
+    }
+}
+
